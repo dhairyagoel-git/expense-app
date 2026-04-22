1a1ff529a3906ee984a80110a0fa567d04e9eb4f
dhairya <dhairyagoel777@gmail.com>
2026-04-22 21:52:00 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const expenseRoutes = require("./routes/expenses");
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:3000","https://expense-app-backend-5fh9.onrender.com" credentials: true }));
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
